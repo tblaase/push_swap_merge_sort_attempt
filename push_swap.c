101b70df5cd0e276170bfdbba04a0d5b394fc22d
tblaase <tblaase@student.42heilbronn.de>
2021-09-13 21:34:52 +0200

    cleaned the main function and added the checker

--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,12 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 12:15:14 by tblaase           #+#    #+#             */
-/*   Updated: 2021/09/06 16:28:44 by tblaase          ###   ########.fr       */
+/*   Updated: 2021/09/13 21:27:42 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-//
-// #include <fcntl.h>
-#include <stdio.h>
-//
+
 int	main(int argc, char **argv)
 /* get the input from the user
 ** create linked list off of that
@@ -29,38 +26,14 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	argc -= 1;
-	// for testing purpose only /////////////////////
-	// argc = 4;
-	// int i = 1;
-	// int fd = open("input.txt", O_RDONLY);
-	// char *buff;
-	// while (i <= 119)
-	// {
-	// int x = 1;
-	// while (x <= 5)
-	// {
-	// 	buff = ft_calloc(2, 1);
-	// 	read(fd, buff, 1);
-	// 	argv[x] = ft_strdup(buff);
-	// 	x++;
-	// }
-	// stack_a = ft_fill_list(argc, argv);
-	// stack_b = ft_fill_list(4, (argv + 4));
-	//
 	stack_a = ft_fill_list(argc, argv);
 	// ft_display_list(stack_a);
 	// ft_display_list(stack_b);
 	if (ft_if_sorted_a(argc, &stack_a) == 1)
 		return (0);
-	// ft_big_sort_a(argc, &stack_a, &stack_b);
 	ft_sort(argc, &stack_a, &stack_b);
 	// ft_display_list(stack_a);
 	// ft_display_list(stack_b);
-	//
-	// printf("\n%d\n\n", i);
-	// i++;
-	// }
-	//
 	// system("leaks push_swap");
 	// fscanf(stdin, "c");
 	return (0);
